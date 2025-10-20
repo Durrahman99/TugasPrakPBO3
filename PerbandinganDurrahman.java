83c791415205c0e26310d6fa5c34649149b57c9c
Durrahman99 <mamatsuhardi99@gmail.com>
2025-10-20 16:15:13 +0700

    Add files via upload

---
+++ PerbandinganDurrahman.java
@@ -0,0 +1,22 @@
+package praktikum3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class PerbandinganDurrahman {
+ public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Input nama (Scanner): ");
+        String nama1 = sc.nextLine();
+
+        System.out.print("Input nama (BufferedReader): ");
+        String nama2 = br.readLine();
+
+        System.out.println("Nama 1: " + nama1);
+        System.out.println("Nama 2: " + nama2);
+    }    
+}
